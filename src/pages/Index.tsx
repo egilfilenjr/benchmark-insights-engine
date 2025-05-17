66d7164cf4462570bd354beffc6af6784d477ba8
egilfilenjr <64110063+egilfilenjr@users.noreply.github.com>
2025-05-17 13:00:33 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
 import HeroSection from "../components/home/HeroSection";
+import AecrDemo from "../components/home/AecrDemo";
 import FeatureTeasers from "../components/home/FeatureTeasers";
 import SocialProof from "../components/home/SocialProof";
-import AecrDemo from "../components/home/AecrDemo";
 import IntegrationsCarousel from "../components/home/IntegrationsCarousel";
 import PlanSelector from "../components/home/PlanSelector";
 import FooterCTA from "../components/home/FooterCTA";
@@ -23,8 +23,10 @@ export default function Index() {
 
   return (
     <MainLayout>
+      {/* 1. Hero Section */}
       <HeroSection />
 
+      {/* CTA Buttons */}
       <div className="flex justify-center p-6 bg-white">
         <div className="flex gap-4">
           <Button asChild size="lg" variant="default">
@@ -36,11 +38,22 @@ export default function Index() {
         </div>
       </div>
 
+      {/* 2. AECR Score™ Demo */}
       <AecrDemo />
+
+      {/* 3. Feature Teasers */}
       <FeatureTeasers />
+
+      {/* 4. Social Proof / Testimonials */}
       <SocialProof />
+
+      {/* 5. Integrations Carousel */}
       <IntegrationsCarousel />
+
+      {/* 6. Plan Selector Preview */}
       <PlanSelector />
+
+      {/* 7. Footer CTA Block */}
       <FooterCTA />
     </MainLayout>
   );
